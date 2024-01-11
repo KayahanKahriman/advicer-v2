3df466392af83eca7c675177f7758f2054fe4f26
Kayahan Kahrıman <kayahan@kayahankahriman.com>
2024-01-11 21:40:49 +0300

    page.php layout

--- page.php
+++ page.php
@@ -1,21 +1,15 @@
 <?php get_header();
-if (!is_home() && !is_front_page()) : ?>
-  <section class="breadcrumbs-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <?php custom_breadcrumbs(); ?>
-        </div>
-      </div>
-    </div>
-  </section>
-<?php endif; ?>
+?>
+
+
 <section class="content-wrapper">
   <div class="container">
     <div class="row">
-      <?php while (have_posts()) : the_post();
-        get_template_part('template-parts/page/content', 'page');
-      endwhile; ?>
+      <div class="col-12">
+        <?php while (have_posts()) : the_post();
+          get_template_part('template-parts/page/content', 'page');
+        endwhile; ?>
+      </div>
     </div>
   </div>
 </section>
